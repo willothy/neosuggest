0ae0bef648aab45ba746bb1403872610964f7071
Will Hopkins <willothyh@gmail.com>
2023-03-15 16:11:29 -0700

    WIP

--- src/sources/pwd.rs
+++ src/sources/pwd.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env::current_dir;
 use std::path::PathBuf;
 
@@ -18,18 +19,18 @@ impl Source for Pwd {
 
     async fn source(&self, search_word: &str) -> Option<String> {
         let split = search_word.split_whitespace();
-        let word = if split.clone().count() > 1 {
-            split.last()?
+        let pwd = current_dir().ok()?;
+        let (mut path, word) = if split.clone().count() > 1 {
+            let word = split.last()?;
+            (PathBuf::from(word), word)
         } else {
-            ""
+            (pwd.clone().unexpand()?, "")
         };
         let rest = search_word[..search_word.len() - word.len()].trim_end();
-        let pwd = current_dir().ok()?;
-        let mut path = PathBuf::from(word);
         let home_abbr = path.starts_with("~");
         path = path.expand()?;
         let len = path.components().count();
-        let (mut base_path, mut search) = if len > 1 {
+        let (base_path, mut search) = if len > 1 && word != "" {
             let search = path
                 .components()
                 .last()
@@ -48,7 +49,7 @@ impl Source for Pwd {
         };
 
         let mut show_next = false;
-        let path = if path.exists() && word.ends_with('/') {
+        let path = if search == "" || (path.exists() && word.ends_with('/')) {
             show_next = true;
             path
         } else {
@@ -60,7 +61,11 @@ impl Source for Pwd {
             .ok()?
             .build()
             .ok()?;
-        let start = search.chars().nth(0)?;
+        let start = if show_next {
+            ' '
+        } else {
+            search.chars().nth(0)?
+        };
         let entries = WalkBuilder::new(&path)
             .max_depth(Some(1))
             .standard_filters(true)
@@ -68,69 +73,77 @@ impl Source for Pwd {
             .hidden(!search.starts_with('.'))
             .filter_entry(move |e| show_next || e.file_name().to_string_lossy().starts_with(start))
             .build()
-            .filter_map(|e| e.ok().map(|v| v.file_name().to_string_lossy().to_string()))
+            .filter_map(|e| {
+                e.ok().map(|v| {
+                    (
+                        v.file_name().to_string_lossy().to_string(),
+                        (
+                            v.metadata().ok().and_then(|m| m.accessed().ok()),
+                            v.path().is_dir(),
+                        ),
+                    )
+                })
+            })
             .skip(1)
-            .collect::<Vec<_>>();
-
-        if show_next || search == "" && !entries.is_empty() {
-            let t = entries.iter().find(|s| !s.starts_with('.'))?.clone();
-            search = t.to_owned();
+            .collect::<HashMap<_, _>>();
+        if (show_next || search == "") && !entries.is_empty() {
+            search = entries.keys().find(|s| !s.starts_with('.'))?.clone();
         }
         let slen = search.len();
         let threshold = if slen > 7 { 0.7 } else { 0.10 * slen as f32 };
+
         let mut res = CorpusBuilder::new()
             .arity(2)
-            .fill(entries)
+            .fill(entries.keys())
             .finish()
             .search(&*search, threshold);
-        if let Some(search_res) = res.first_mut() {
-            let matching = if show_next {
-                return Some(format!(
-                    "{} {}{}",
-                    rest,
-                    &word,
-                    std::mem::take(&mut search_res.text)
-                ));
-            } else {
-                std::mem::take(&mut search_res.text)
-            };
+        let res_count = res.len();
+        if res_count == 0 {
+            return None;
+        } else if res_count > 2 {
+            // Average similarity
+            let mut mean_similarity = 0.;
+            res.iter().for_each(|r| mean_similarity += r.similarity);
+            mean_similarity /= res_count as f32;
+            // Filter by above average match
+            res = res
+                .into_iter()
+                .filter(|r| r.similarity > mean_similarity)
+                .collect();
+        }
+        // Sort by similarity
+        res.sort_unstable_by(|x, y| entries.get(&x.text).cmp(&entries.get(&y.text)));
 
-            let result = if base_path == pwd {
+        if let Some(search_res) = res.first_mut() {
+            let matching = std::mem::take(&mut search_res.text);
+            let is_dir = entries.get(&matching).map(|(_, d)| *d).unwrap_or(false);
+
+            if show_next {
+                let dir = if is_dir { "/" } else { "" };
+                return Some(format!("{} {}{}", rest, matching, dir,));
+            }
+
+            let path = if base_path == pwd {
                 let p = PathBuf::from(&matching);
-                let (is_dir, p) = if home_abbr {
-                    (p.is_dir(), p.unexpand()?.to_string_lossy().to_string())
+                if home_abbr {
+                    p.unexpand()?.to_string_lossy().to_string()
                 } else {
-                    (p.is_dir(), p.to_string_lossy().to_string())
-                };
-                if rest.is_empty() {
-                    p + if is_dir { "/" } else { "" }
-                } else {
-                    rest.to_owned() + " " + &p + if is_dir { "/" } else { "" }
+                    p.to_string_lossy().to_string()
                 }
             } else {
                 let p = base_path.join(matching);
-                let (is_dir, p) = if home_abbr {
-                    (p.is_dir(), p.unexpand()?.to_string_lossy().to_string())
+                if home_abbr {
+                    p.unexpand()?.to_string_lossy().to_string()
                 } else {
-                    (p.is_dir(), p.to_string_lossy().to_string())
-                };
-                if rest.is_empty() {
-                    p + if is_dir { "/" } else { "" }
-                } else {
-                    rest.to_owned() + " " + &p + if is_dir { "/" } else { "" }
+                    p.to_string_lossy().to_string()
                 }
             };
-
-            Some(
-                result, // + &format!(
-                       //     " \n{}",
-                       //     res.iter()
-                       //         .skip(1)
-                       //         .map(|v| v.text.clone())
-                       //         .collect::<Vec<_>>()
-                       //         .join("\n")
-                       // ),
-            )
+            let result = if rest.is_empty() {
+                path + if is_dir { "/" } else { "" }
+            } else {
+                rest.to_owned() + " " + &path + if is_dir { "/" } else { "" }
+            };
+            Some(result)
         } else {
             None
         }
